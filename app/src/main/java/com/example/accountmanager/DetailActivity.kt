39290a2f1c1e9ef74270c8ec0b9490e1edb16a20
Minakshi <minakshi@bridgelogicsoftware.com>
2024-06-01 13:00:00 +0530

    account manager app 01-2024

--- app/src/main/java/com/example/accountmanager/DetailActivity.kt
+++ app/src/main/java/com/example/accountmanager/DetailActivity.kt
@@ -1,8 +1,12 @@
 package com.example.accountmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.deishelon.roundedbottomsheet.RoundedBottomSheetDialog
 import com.example.accountmanager.databinding.ActivityDetailBinding
+import com.example.accountmanager.databinding.AddottomsheettemBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -10,5 +14,22 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.icAdd.setOnClickListener {
+            addBottomsheet()
+        }
+    }
+    private fun addBottomsheet() {
+        val mBottomSheetDialog = this?.let { RoundedBottomSheetDialog(it) }
+        val bottomBinding: AddottomsheettemBinding =
+            AddottomsheettemBinding.inflate(LayoutInflater.from(this))
+        mBottomSheetDialog?.setContentView(bottomBinding.root)
+        mBottomSheetDialog?.show()
+        bottomBinding.icClose.setOnClickListener {
+            mBottomSheetDialog?.dismiss()
+        }
+        bottomBinding.cardAdd.setOnClickListener {
+            mBottomSheetDialog?.dismiss()
+        }
     }
 }
---
+++ app/src/main/java/com/example/accountmanager/adapter/DetailRecyclerAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.accountmanager.adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DetailRecyclerAdapter{
+
+
+}
